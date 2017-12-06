511f7fdf2556f0ba095823f4d46e4edbcf6bd086
Kara Cavanaugh <karacav@msn.com>
2017-12-06 16:12:50 -0600

    finished site

--- app/app.js
+++ app/app.js
@@ -1,10 +1,32 @@
-angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider, $urlRouterProvider ) {
-  $stateProvider
-    .state('home',{
-      url:'/',
-      templateUrl: "app/about/aboutTmpl.html"
-    });
-
-  $urlRouterProvider
-    .otherwise('/');
-});
+angular
+  .module('devmtnTravel', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', { url: '/', templateUrl: 'app/about/aboutTmpl.html' })
+      .state('packages', {
+        url: '/packages/:country',
+        templateUrl: 'app/packages/packagesTmpl.html',
+        controller: 'packagesCtrl'
+      })
+      .state('locations', {
+        url: '/locations',
+        templateUrl: 'app/locations/locationsTmpl.html',
+        controller: 'locationsCtrl'
+      })
+      .state('adventurers', {
+        url: '/adventurers',
+        templateUrl: 'app/about/adventurers/adventurersTmpl.html',
+        parent: 'home'
+      })
+      .state('booked', {
+        url: '/booked/:id',
+        templateUrl: 'app/booked/bookedTmpl.html',
+        controller: 'bookedCtrl'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'app/about/contact/contactTmpl.html',
+        parent: 'home'
+      });
+    $urlRouterProvider.otherwise('/');
+  });
